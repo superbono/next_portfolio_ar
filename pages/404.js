f33f7209fd20f648c18eb08039447c22972310f5
superbono <fr.dev.ar@gmail.com>
2024-04-10 00:35:01 +0900

    02. add page

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div style={{ margin: "250px auto", textAlign: "center" }}>
+      <h2>404</h2>
+      <h5>Not Found Page</h5>
+    </div>
+  );
+}
---
+++ pages/project.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+export default function Project() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <h2>Project</h2>
+      </main>
+    </div>
+  );
+}
