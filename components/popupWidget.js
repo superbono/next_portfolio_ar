6ead9a13a7648d1091cf6d3112da4a0f901da56e
superbono <fr.dev.ar@gmail.com>
2024-04-13 23:54:43 +0900

    22. tailwind-css기반 배포준비

--- components/popupWidget.js
+++ components/popupWidget.js
@@ -6,7 +6,10 @@ const PopupWidget = () => {
     <div>
       <>
         <Link href="#">
-          <a className="fixed z-40 flex items-center text-slate-300 hover:text-white justify-center transition duration-300 bg-slate-400 rounded-full shadow-lg right-5 bottom-5 w-14 h-14 focus:outline-none hover:bg-slate-600">
+          <a
+            style={{ color: "#fff" }}
+            className="fixed z-40 flex items-center text-white hover:text-white justify-center transition duration-300 bg-amber-500 rounded-full shadow-lg right-5 bottom-5 w-14 h-14 focus:outline-none hover:bg-slate-950"
+          >
             TOP
           </a>
         </Link>
